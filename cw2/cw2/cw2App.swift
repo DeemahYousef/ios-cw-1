512dfe1e6abf10af09ade1175dc73a6b96af81c0
DeemahYousef <dalkhaledi12345@hotmail.com>
2022-03-03 14:28:05 +0300

    done

---
+++ cw2/cw2/cw2App.swift
@@ -0,0 +1,17 @@
+//
+//  cw2App.swift
+//  cw2
+//
+//  Created by DIMA ALKHALEDI on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
